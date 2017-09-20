38d473c202c05b49a96a1c940cb4413ed77c5fd9
Lefty <brjebrow@umail.iu.edu>
2017-09-20 16:59:36 -0400

    Added Cards & Button Functionality
    Added a click listener to Fav button that switches icon from outline to solid

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,51 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    cards = [
+        {
+            imgPath: "forest.jpg",
+            title: "Forest Bench & Lake",
+            desc: "I really wish I was here right now instead.",
+            favorited: false
+        },
+        {
+            imgPath: "forest2.jpg",
+            title: "Hazy Forest",
+            desc: "I really wish I was here right now instead.",
+            favorited: false
+        },
+        {
+            imgPath: "forest3.jpg",
+            title: "Aesthetic Sunlit Trees",
+            desc: "I really wish I was here right now instead.",
+            favorited: false
+        }
+    ];
 
-  }
+    constructor(public navCtrl: NavController) {
+
+    }
+
+    clickFav(card) {
+        // if (card) {
+        //     card.favorited = !card.favorited;
+        // }
+
+        let index = this.cards.indexOf(card);
+
+        if (index > -1) {
+            if (card.favorited) {
+                this.cards[index].favorited = false;
+            } else {
+                this.cards[index].favorited = true;
+            }
+        }
+    }
 
 }
